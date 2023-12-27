9f039a4c51e982a9c098d32951c44ff95ca38c9a
jonah.zeng <302826183@qq.com>
2023-12-27 13:01:15 +0800

    add texture chapter

--- chapter5/src/shader.fs
+++ chapter5/src/shader.fs
@@ -2,10 +2,13 @@
 out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
-uniform sampler2D ourTexture;
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+uniform float mixWeight;
 
 void main()
 {
 	// FragColor = vec4(ourColor, 1.0f);
-	FragColor = texture(ourTexture, TexCoord);
+	// FragColor = texture(texture1, TexCoord);
+	FragColor = mix(texture(texture0, TexCoord), texture(texture1, vec2(1.0f-TexCoord.x, TexCoord.y)), mixWeight);
 };
